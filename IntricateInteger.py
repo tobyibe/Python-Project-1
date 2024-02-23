4bf5756a2caf105e8684263b95f899e7a4e3cb8b
Sufyan11104 <sufyanarabic@gmail.com>
2024-02-23 05:17:35 +0000

    Update IntricateInteger.py
    Added Requirement 1 and Requirement 2

--- IntricateInteger.py
+++ IntricateInteger.py
@@ -1,5 +1,7 @@
 import math
 class IntricateInteger:
+    # Represents an intricate integer with modular arithmetic operations.
+
     def __init__(self, object, mod, mul):
         if (not (isinstance(object, int) and isinstance(mod, int) and isinstance(mul, int))):
             raise ValueError("The value, modulus and multiplier must all be integers")
@@ -13,13 +15,34 @@ class IntricateInteger:
 
     # overwrite "print"
     def __str__(self):
+        # Returns a string representation of the IntricateInteger object.
         return "<"+str(self.object)+" mod "+str(self.mod)+" | "+str(self.mul)+">"
 
     # define "*"
     def __mul__(self,other):
+        # Multiplies two IntricateInteger objects.
         if ( self.mod != other.mod or self.mul != other.mul):
             raise ArithmeticError("The two intricate integers must have the same modulus and multiplier")
         return IntricateInteger((self.object + other.object + self.mul * (math.lcm(self.object, other.object))) % self.mod, self.mod, self.mul)
 
-a = IntricateInteger(3,5,4)
-b = IntricateInteger(2,5,4)
+def has_intricate_peculiar_property(n, alpha):
+    # Checks if the intricate peculiar property holds for given n and alpha.
+    for x in range(n):
+        ii_x = IntricateInteger(x, n, alpha)
+        result = ii_x * ii_x
+        if result.object != x:
+            return False
+    return True
+
+def has_commutative_intricate_multiplication(n, alpha):
+     # Checks if multiplication is commutative for given n and alpha.
+    for x in range(n):
+        for y in range(n):
+            ii_x = IntricateInteger(x, n, alpha)
+            ii_y = IntricateInteger(y, n, alpha)
+            result_xy = ii_x * ii_y
+            result_yx = ii_y * ii_x
+            if result_xy.object != result_yx.object:
+                return False  # Not commutative for these values
+    return True  # Commutative for all x, y in Zn
+
